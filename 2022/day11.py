0226f413ac17d188316c54b666bad4c36921c5c4
re-redo <114634128+re-redo@users.noreply.github.com>
2022-12-27 16:09:26 -0800

    refined solution with class

--- 2022/day11.py
+++ 2022/day11.py
@@ -1,74 +1,95 @@
+class Monkey:
+    def __init__(self, items, test, tr, fa, op):
+        self.items = items
+        self.test = test
+        self.tr = tr
+        self.fa = fa
+        self.op = op
+        self.activity = 0
 
-# int(x/y) rounds down
+    def update_count(self):
+        self.activity += 1
+
+    def get_test(self):
+        return self.test
+
+    def get_items(self):
+        return self.items
+
+    def get_op(self):
+        return str(self.op)
+
+    def get_activity(self):
+        return int(self.activity)
+
+    def test_item(self, worry: int):
+        w = worry // 3
+        if w % self.test == 0:
+            return self.tr, w
+        else:
+            return self.fa, w
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def clear(self):
+        self.items = []
+
 
 # read file input, split on each \n
 with open('.\\inputs\\day11.txt', "r") as f:    
     lines = f.read().splitlines()
 
+
 # declare global 
-monkeys_have = {}
-monkeys_op = []
-monkeys_test = []
-activity = []
+monkeys = []
+
 
 # read input
 for x in range(0, len(lines), 7):
-    l1 = lines[x]
-    key = int(l1[-2])
-    activity.append(0)
-
-    # put starting items value in dict
+    # create list of starting items/worries
     items = []
-    l2 = lines[x+1].split(":")[1]
-    for i in l2.split(","):
+    l1 = lines[x+1].split(":")[1]
+    for i in l1.split(","):
         items.append(int(i))
-    monkeys_have[key] = items
-    
+
     # put operation string (later eval()) in list
-    l3 = lines[x+2].split("=")[1]
-    if "*" in l3:
-        l3_split = l3.split("*")
+    l2 = lines[x+2].split("=")[1]
+    if "*" in l2:
+        l2_split = l2.split("*")
         op = "*"
     else:
-        l3_split = l3.split("+")
+        l2_split = l2.split("+")
         op = "+"
-    monkeys_op.append(l3_split[0].strip() + op + l3_split[1].strip())
-
-
-    # assign conditions
-    l4 = lines[x+4]
-    l5 = lines[x+5]
+    op = l2_split[0].strip() + op + l2_split[1].strip()
 
     # create function to test where to throw for each
-    def f(worry = 0, x = x, l4=l4, l5=l5):
-        y = int(worry / 3)
-        # if true throw to monkey listed on line 4 otherwise line 5
-        if y % int(lines[x+3].split("y")[1]) == 0:
-            return int(l4[-1])
-        else:
-            return int(l5[-1])
-    monkeys_test.append(f)
+    div = int(lines[x+3].split("y")[1])
+    
+    monkeys.append(Monkey(items=items, op=op, test=div, \
+        tr=int(lines[x+4][-1]), fa=int(lines[x+5][-1])))
 
-# print(monkeys_have)
-# print(monkeys_op)
-#print(monkeys_test[3](worry=105))
 
 # for 20 rounds
 for round in range(0, 20):
     # for each monkey in order
-    for n in range(0, len(monkeys_have)): 
+    for n in range(0, len(monkeys)): 
         # for each item the monkey holds in order
-        start_len = len(monkeys_have[n])
+        old_list = monkeys[n].get_items()
+        start_len = len(old_list)
         for i in range(0, start_len):
-            activity[n] += 1
-            old = monkeys_have[n][i]
-            new = eval(monkeys_op[n])
-            next_monkey = monkeys_test[n](worry = new)
-            #print("{} becomes {} and goes to monkey {}".format(old, int(new/3), next_monkey))
-            monkeys_have[next_monkey].append(int(new/3))
-        monkeys_have[n] = []
-    print(activity)
-print(monkeys_have)
+            monkeys[n].update_count()
+            op = monkeys[n].get_op()
+            old = old_list[i]
+            new = eval(op)
+            next_monkey, new_worry = monkeys[n].test_item(new)
+            monkeys[next_monkey].add_item(new_worry)
+        monkeys[n].clear()
 
-activity.sort()
-print(activity[-1] * activity[-2])
+highest = []
+for m in monkeys:
+    highest.append(m.get_activity())
+highest.sort()
+print(highest[-1] * highest[-2])
+
+
